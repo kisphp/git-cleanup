d9b0a269de25afaf75ce01b0499a2f410852289b
Kisphp <mariusbogdan83@gmail.com>
2016-06-21 19:50:06 +0000

    added command

--- src/Command/ClearBranchesCommand.php
+++ src/Command/ClearBranchesCommand.php
@@ -2,7 +2,22 @@
 
 namespace Kisphp\Command;
 
-class ClearBranchesCommand
+use Cilex\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearBranchesCommand extends Command
 {
+    protected function configure()
+    {
+        $this->setName('clean:branches')
+            ->setDescription('Remove merged branches')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('hello world, remove branches');
+    }
 
 }
